39154abcb54295d11e063945e7a0fc727dadcd06
iamsatyapati <iamsatyapati@gmail.com>
2024-04-27 13:44:32 +0530

    props and component implemented

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import Header from "./Header";
+import Item from "./Item";
+
 function App() {
   return (
+    <div> 
+      <div><Navbar /></div>
     <h1>Hello World!</h1>
+    <div><Footer /></div>
+    <Item name="item1"/>
+    <Item name="item2"/>
+    <Item name="item3"/>
+    <Header />
+    </div>
   );
 }
 
