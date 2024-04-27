97ea9af5c22d08ba4e4cbda49821ff6830165e62
iamsatyapati <iamsatyapati@gmail.com>
2024-04-27 17:02:17 +0530

    state implemented

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Navbar from "./Navbar";
 import Footer from "./Footer";
 import Header from "./Header";
 import Item from "./Item";
+import Message from "./Message";
+import Counter from "./Counter";
 
 function App() {
   return (
@@ -11,8 +13,10 @@ function App() {
     <div><Footer /></div>
     <Item name="item1"/>
     <Item name="item2"/>
-    <Item name="item3"/>
+    <Item name="item3" price="500"> <p> This is the explanation of the item</p> </Item>
     <Header />
+    <Message />
+    <Counter />
     </div>
   );
 }
--- src/Item.js
+++ src/Item.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 
-const Item = props => <h1>{props.name}</h1>
+// const Item = props => <div><h1>{props.name}</h1> <h2>{props.price}</h2><p>{props.children}</p></div>
 
-
+class Item extends React.Component{
+    render(){
+        return <div><h1>{this.props.name}</h1> <h2>{this.props.price}</h2><p>{this.props.children}</p></div>
+    }
+}
 export default Item;
